9f414bf2cfeeb859b9337c4b6759026bb59dad82
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-16 13:20:05 -0700

    updated main which creates Query struct out of args and runs the search.

--- 01_chapter/grrs/src/main.rs
+++ 01_chapter/grrs/src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::process;
+
+use grrs;
+use grrs::Query;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Query::new(env::args()).unwrap_or_else(|err| {
+        //eprintln prints to stderr
+        eprintln!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = grrs::run(config) {
+        //eprintln prints to stderr
+        eprintln!("Application error {}.", e);
+        process::exit(1);
+    }
 }
