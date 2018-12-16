3e2d53ead356cf1fc4713977bfab7b395ce2bbf4
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-16 12:15:35 -0700

    Added search and run capabilities in the lib.

--- 01_chapter/grrs/src/lib.rs
+++ 01_chapter/grrs/src/lib.rs
@@ -6,19 +6,45 @@ pub struct Query {
 }
 
 impl Query {
+    //new will require env::Args and will produce a Result
+    //When Ok it returns Query
+    //When in Error it returns Err with error message
     pub fn new(mut args: env::Args) -> Result<Query, &'static str> {
+        //first argument (0th) is the program name, should be ignored.
         args.next();
 
+        //check if user provided pattern to search
         let pattern = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query stirng."),
         };
-
+        //check if user provided path as the second argument
         let path = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a file name."),
         };
-
+        //if pattern and path were provided by the user, construct Query Struct
         Ok(Query { pattern, path })
     }
+}
+
+use std::error::Error;
+use std::fs;
+pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
+    //get the contents of the file
+    let content = fs::read_to_string(query.path)?;
+    //search for the result
+    let results = search(&query.pattern, &content);
+    //print result line by line
+    for line in results {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
